0fd99b8756a1a9bf8a5b072409c82af7fbd9f4cf
Kishen S Jain <jain.skishen@gmail.com>
2026-08-25 19:45:59 +0530

    feat(middlewares): add asyncHandler middleware for handling asynchronous requests

---
+++ server/src/middlewares/asyncHandler.ts
@@ -0,0 +1,20 @@
+import { RequestHandler } from "express";
+
+const asyncHandler =
+  (fn: RequestHandler): RequestHandler =>
+  (req, res, next) =>
+    Promise.resolve(fn(req, res, next)).catch(next);
+
+export default asyncHandler;
+
+/*
+(req, res, next) => {
+   // do something
+}
+This is a typical Express request handler.
+
+.catch(next) is conceptually 
+.catch((error) => {
+  next(error);
+})
+*/
