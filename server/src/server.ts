900dfdb3a5c989c22cbe7d89e0ad4a83eb56eb0a
Kishen S Jain <jain.skishen@gmail.com>
2026-08-25 20:04:48 +0530

    feat(middleware): add cookie-parser for handling cookies and update server configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middlewares/error.middleware.js";
 
 dotenv.config({debug: true});
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+
+// Express's middleware for handling JSON request bodies.
+app.use(express.json({ limit: "1mb" }));
+// Express's middleware for handling URL-encoded request bodies like form data.
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+// Express's middleware for handling cookies
+app.use(cookieParser());
 
 app.get("/", (_req, res) => {
   res.json({
@@ -17,7 +27,8 @@ app.get("/", (_req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+app.use(notFound);
+app.use(errorHandler);
 
 const startServer = async (): Promise<void> => {
   await connectDB();
