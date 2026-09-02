687bef5e654593d1f519e681e66e87980703621b
Kishen S Jain <jain.skishen@gmail.com>
2026-09-02 16:47:03 +0530

    feat(server): hide Express header and configure cross-origin resource policy and add routes to server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/user.routes.js";
+import movieRoutes from "./routes/movie.routes.js";
+import genreRoutes from "./routes/genre.routes.js";
 import { errorHandler, notFound } from "./middlewares/error.middleware.js";
 
 dotenv.config({debug: true});
@@ -12,7 +17,19 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
+// Disable the X-Powered-By header
+app.disable("x-powered-by");
+//helmet is an Express middleware that sets various HTTP security headers.
+app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 300,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
+  }),
+);
 
 // Express's middleware for handling JSON request bodies.
 app.use(express.json({ limit: "1mb" }));
@@ -27,6 +44,9 @@ app.get("/", (_req, res) => {
   });
 });
 
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/genre", genreRoutes);
+app.use("/api/v1/movies", movieRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
