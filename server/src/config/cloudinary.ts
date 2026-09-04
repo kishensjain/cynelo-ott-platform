26edd4cc25ba107c419a9309efdcf0126b1fc3e5
Kishen S Jain <jain.skishen@gmail.com>
2026-09-04 15:35:23 +0530

    feat(cloudinary): import dotenv and configure environment variables for Cloudinary

--- server/src/config/cloudinary.ts
+++ server/src/config/cloudinary.ts
@@ -1,9 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY, //Identifies your app/account
   api_secret: process.env.CLOUDINARY_API_SECRET, //Proves your app is authorized
 });
-
 export default cloudinary;
