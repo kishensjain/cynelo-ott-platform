e260f52792fd53786bc1159d68ce71dd3de3eab3
Kishen S Jain <jain.skishen@gmail.com>
2026-09-15 16:46:28 +0530

    fix: uncomment server port and listen logic in server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ import { errorHandler, notFound } from "./middlewares/error.middleware.js";
 dotenv.config();
 
 const app = express();
-// const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 // Disable the X-Powered-By header
@@ -55,9 +55,9 @@ app.use(errorHandler);
 const startServer = async (): Promise<void> => {
   await connectDB();
 
-  // app.listen(PORT, () => {
-  //   console.log(`Cynelo API running on http://localhost:${PORT}`);
-  // });
+  app.listen(PORT, () => {
+    console.log(`Cynelo API running on http://localhost:${PORT}`);
+  });
 };
 
 startServer();
